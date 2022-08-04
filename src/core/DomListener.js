bdafced9535567e4359c600a6d709b71e1b3e585
bogdan-save-dev <tradeexample99@gmail.com>
2022-08-04 14:17:18 +0300

    method removeListners

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -9,11 +9,19 @@ export class DomListener {
   initDOMListeners() {
     this.listeners.forEach((listener) => {
       const method = getMethodName(listener)
-
+      if (!this[method]) {
+        throw new Error(`method ${method} isnt  implemented in ${this.name} `)
+      }
+      this[method] = this[method].bind(this)
       this.$root.on(listener, this[method])
     })
   }
-  removeDOMListeners() {}
+  removeDOMListeners() {
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener)
+      this.$root.off(listener, this[method])
+    })
+  }
 }
 
 function getMethodName(eventName) {
